3f5ec731f9404c6da94b0096b19eb9f4658e93de
Ulose <44412197+Ulose@users.noreply.github.com>
2019-10-08 23:00:56 +0200

    initial commit

---
+++ writetofile.py
@@ -0,0 +1,23 @@
+from html5print import HTMLBeautifier
+import sys
+import io
+
+WORKING_PATH = 'D:\Git\writetofile/'
+
+
+def operation_on_file(*args):
+    file_path = WORKING_PATH + args[0]
+    if len(args) > 1:
+        operation = 'w+'
+    else:
+        operation = 'r'
+
+    with open(file_path, operation) as operation_place:
+        if len(args) > 1:
+            operation_place.write(args[1])
+        else:
+            return HTMLBeautifier.beautify(''.join(operation_place.readlines()))
+
+
+strin = operation_on_file('toread.html')
+operation_on_file('towrite.html', strin)
